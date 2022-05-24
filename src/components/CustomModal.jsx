1d82447a78e682cf34d21d10ebe5022e5ce0fd73
EdgarOSF <edgaromarsf@gmail.com>
2022-05-24 08:02:13 -0500

    finish proyect

---
+++ src/components/CustomModal.jsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from 'reactstrap';
+
+const CustomModal = ({ activeItem, setActiveItem, ...props }) => {
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+
+    if (e.target.type === 'checkbox') {
+      value = e.target.checked;
+    }
+
+    const activeItemSelected = { ...activeItem, [name]: value };
+
+    setActiveItem(activeItemSelected);
+  };
+
+  const { toggle, onSave } = props;
+
+  return (
+    <Modal isOpen={true} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
+      <ModalBody>
+        <Form>
+          <FormGroup>
+            <Label for="todo-title">Title</Label>
+            <Input
+              type="text"
+              id="todo-title"
+              name="title"
+              value={activeItem.title}
+              onChange={handleChange}
+              placeholder="Enter Todo Title"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="todo-description">Description</Label>
+            <Input
+              type="text"
+              id="todo-description"
+              name="description"
+              value={activeItem.description}
+              onChange={handleChange}
+              placeholder="Enter Todo description"
+            />
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input
+                type="checkbox"
+                name="completed"
+                checked={activeItem.completed}
+                onChange={(e) => handleChange(e)}
+              />
+              Completed
+            </Label>
+          </FormGroup>
+        </Form>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="success" onClick={() => onSave(activeItem)}>
+          Save
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default CustomModal;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import reactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import App from './App';
 
-const app = <img src="/logo.svg"></img>;
+const app = <App />;
 const container = document.getElementById('root');
 
 reactDom.render(app, container);
